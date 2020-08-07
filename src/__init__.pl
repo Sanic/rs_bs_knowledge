2a56c13c2e6a8eec53c5c93ff3c9c75a4d3b644a
Sanic <Sanic@users.noreply.github.com>
2020-08-07 14:04:33 +0200

    trying to work with ontologies

--- src/__init__.pl
+++ src/__init__.pl
@@ -1,10 +1,9 @@
 :- register_ros_package(knowrob).
 :- use_module(library('db/tripledb')).
+:- use_module(library('semweb/rdf_db'),  [ rdf_register_ns/3 ]).
 
 :- tripledb_load('package://rs_bs_knowledge/owl/rs_bs_knowledge.owl').
-
-% :- owl_parser:owl_parse('package://rs_bs_knowledge/owl/rs_bs_knowledge.owl').
-% tripledb_load('http://www.ease-crc.org/ont/EASE-IO.owl'
+:- rdf_register_ns(rs_bs_knowledge,'http://github.com/sanic/rs_bs_knowledge/owl/rs_bs_knowledge.owl#',[keep(true)]).
 
 :- use_module(library(rs_bs_knowledge)).
 
