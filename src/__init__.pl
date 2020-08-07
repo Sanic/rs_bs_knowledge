e2f5d22481037b19464c0ff3e6c4c4b83cc3d7e4
Sanic <Sanic@users.noreply.github.com>
2020-08-07 12:12:10 +0200

    current state

--- src/__init__.pl
+++ src/__init__.pl
@@ -1,2 +1,10 @@
 :- register_ros_package(knowrob).
+:- use_module(library('db/tripledb')).
+
+:- tripledb_load('package://rs_bs_knowledge/owl/rs_bs_knowledge.owl').
+
+% :- owl_parser:owl_parse('package://rs_bs_knowledge/owl/rs_bs_knowledge.owl').
+% tripledb_load('http://www.ease-crc.org/ont/EASE-IO.owl'
+
 :- use_module(library(rs_bs_knowledge)).
+
